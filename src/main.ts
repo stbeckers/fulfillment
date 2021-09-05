55c50e53f99d4db7b318d7ace9ff268b97ec1c4e
stevenbeckers <steven.beckers@pwc.com>
2021-09-05 12:39:18 +0200

    feat(task): created a "workflow" for the basic steps to solve the presented task to create an order and perfect pick all pickjobs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,44 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
+import { FulfillmentService } from './app/fulfillment/fulfillment.service';
+import { consumer, orderLineItem } from './data/mock-data';
+import { AuthService } from './app/auth/auth.service';
+import { retry } from 'ts-retry-promise';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createApplicationContext(AppModule);
+
+  const authService = app.get(AuthService);
+  const fulfillmentService = app.get(FulfillmentService);
+
+  // get access
+  const authToken = await authService.getAuthToken();
+
+  // create order
+  const order = await fulfillmentService.createOrder(
+    [orderLineItem],
+    consumer,
+    authToken,
+  );
+
+  // getting the pickjob
+  // need to wrap this with retry as the server has no pickjobs instantly
+  const strippedPickJob = await retry(
+    () => fulfillmentService.getAllPickJobsForOrder(order.id, authToken),
+    {
+      backoff: 'FIXED',
+      retries: 5,
+      delay: 2000,
+    },
+  );
+  console.log(strippedPickJob);
+
+  // set pickjob in progress
+
+  // perfect pick any item
+
+  // close pickjob
+
+  // revoke access
 }
 bootstrap();
