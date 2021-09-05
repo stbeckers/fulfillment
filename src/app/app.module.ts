4b5a122c8b7c6143b626551aecf84def43da31e0
stevenbeckers <steven.beckers@pwc.com>
2021-09-05 12:52:39 +0200

    refactor: cleaned up some unused stuff from the starter project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { FulfillmentModule } from './fulfillment/fulfillment.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [FulfillmentModule, ConfigModule.forRoot(), AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
