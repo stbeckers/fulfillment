acc6feea802b1e46a1dc70fee05f6339c75083fd
stevenbeckers <steven.beckers@pwc.com>
2021-09-05 14:53:45 +0200

    feat(fulfillment): more mockData to test

--- src/data/mock-data.ts
+++ src/data/mock-data.ts
@@ -9,6 +9,13 @@ import {
 } from '../app/fulfillment/models/consumer.model';
 import { LineItemArticle } from '../app/fulfillment/models/shared.model';
 import { v4 } from 'uuid';
+import {
+  DeliveryChannel,
+  PickJob,
+  PickJobDeliveryInformation,
+  PickLineItem,
+  PickStatus,
+} from '../app/fulfillment/models/pick-job.model';
 
 export const lineItemArticle: LineItemArticle = {
   attributes: [],
@@ -18,7 +25,21 @@ export const lineItemArticle: LineItemArticle = {
 
 export const orderLineItem: OrderLineItem = {
   article: lineItemArticle,
-  id: '',
+  id: v4(),
+  quantity: 1,
+  scannableCodes: [],
+  shopPrice: 1,
+};
+
+export const lineItemArticle2: LineItemArticle = {
+  attributes: [],
+  tenantArticleId: v4(),
+  title: 'FIFA 22',
+};
+
+export const orderLineItem2: OrderLineItem = {
+  article: lineItemArticle2,
+  id: v4(),
   quantity: 1,
   scannableCodes: [],
   shopPrice: 1,
@@ -42,7 +63,31 @@ export const mockOrder: Order = {
   consumer: consumer,
   id: v4(),
   orderDate: new Date().toISOString(),
-  orderLineItems: [orderLineItem],
+  orderLineItems: [orderLineItem, orderLineItem2],
   status: OrderStatus.enum.OPEN,
-  version: 0,
+  version: 1,
+};
+
+export const mockPickLineItem: PickLineItem = {
+  article: lineItemArticle,
+  id: v4(),
+  picked: 0,
+  quantity: 5,
+  status: PickStatus.enum.OPEN,
+};
+
+export const mockDeliveryInformation: PickJobDeliveryInformation = {
+  channel: DeliveryChannel.enum.SHIPPING,
+  targetTime: new Date().toISOString(),
+};
+
+export const mockPickJob: PickJob = {
+  deliveryinformation: mockDeliveryInformation,
+  facilityRef: '',
+  id: v4(),
+  orderDate: new Date().toISOString(),
+  pickLineItems: [mockPickLineItem],
+  shortId: 'shortId',
+  status: PickStatus.enum.OPEN,
+  version: 1,
 };
