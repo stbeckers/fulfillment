c5c5101b24c49ea9a4524a71fe1fa85e84609e0c
stevenbeckers <steven.beckers@pwc.com>
2021-09-05 14:54:25 +0200

    feat(task): finally the workflow is complete

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { FulfillmentService } from './app/fulfillment/fulfillment.service';
-import { consumer, orderLineItem } from './data/mock-data';
+import { consumer, orderLineItem, orderLineItem2 } from './data/mock-data';
 import { AuthService } from './app/auth/auth.service';
 import { retry } from 'ts-retry-promise';
 
@@ -14,14 +14,14 @@ async function bootstrap() {
   // get access
   const authToken = await authService.getAuthToken();
 
-  // create order
+  console.log('create order');
   const order = await fulfillmentService.createOrder(
-    [orderLineItem],
+    [orderLineItem, orderLineItem2],
     consumer,
     authToken,
   );
 
-  // getting the pickjob
+  console.log('getting the pickjob');
   // need to wrap this with retry as the server has no pickjobs instantly
   const strippedPickJob = await retry(
     () => fulfillmentService.getAllPickJobsForOrder(order.id, authToken),
@@ -31,13 +31,26 @@ async function bootstrap() {
       delay: 2000,
     },
   );
-  console.log(strippedPickJob);
 
-  // set pickjob in progress
+  console.log('set pickjob in progress');
+  const updatedPickJobs = await Promise.all(
+    strippedPickJob.pickjobs.map(
+      async pickJob =>
+        await fulfillmentService.setPickJobInProgress(
+          pickJob.id,
+          pickJob.version,
+          authToken,
+        ),
+    ),
+  );
 
-  // perfect pick any item
-
-  // close pickjob
+  console.log('perfect pick any item and close pickjob');
+  await Promise.all(
+    updatedPickJobs.map(
+      async pickJob =>
+        await fulfillmentService.pickPerfectAndClosePickJob(pickJob, authToken),
+    ),
+  );
 
   // revoke access
 }
